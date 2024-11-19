50517d99b752c30ea31bbcd6d5961b33553bc544
adriancesurform <adrian.s465656@cesurformacion.com>
2024-11-19 10:14:20 +0100

    post

--- POST&GET/formulario.php
+++ POST&GET/formulario.php
@@ -2,6 +2,11 @@
 var_dump($_POST);
 var_dump($_FILES);
 var_dump($_GET);
+var_dump($_REQUEST);
+var_dump($_SESSION);
+var_dump($_COOKIE);
+var_dump($_ENV);
+var_dump($http_response_header);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
