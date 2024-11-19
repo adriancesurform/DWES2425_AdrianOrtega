ccf6ba0d91804cfa8efaa153bdc2a53c7f2a8b8f
adriancesurform <adrian.s465656@cesurformacion.com>
2024-11-19 10:09:31 +0100

    post

--- POST&GET/formulario.php
+++ POST&GET/formulario.php
@@ -1,9 +1,9 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $nom = $_POST['nom'] ?? 'Vacío';
-    $edat = $_POST['edat'] ?? 'Vacio';
-    $dni = $_POST['dni'] ?? 'Vacío';
+    $nom = $_POST['nom'] ?? 'Inválido';
+    $edat = $_POST['edat'] ?? '';
+    $dni = $_POST['dni'] ?? '';
 
     echo "Nombre: " . htmlspecialchars($nom) . "<br>";
     echo "Edad: " . htmlspecialchars($edat) . "<br>";
