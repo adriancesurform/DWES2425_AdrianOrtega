074be52e53bc8178be3ae72e306ae93ea4f2b78a
adriancesurform <adrian.s465656@cesurformacion.com>
2024-11-19 10:36:09 +0100

    post

--- POST&GET/formulario.php
+++ POST&GET/formulario.php
@@ -1,21 +1,2 @@
 <?php
-
-    $json_data = file_get_contents('php://input'); // Leer el cuerpo de la solicitud
-    $data = json_decode($json_data, true); // Decodificar el JSON a un array asociativo
-
-    // Verificamos si los datos fueron correctamente decodificados
-    if ($data) {
-        // Extraemos las variables
-        $nom = $data['nom'] ?? 'No proporcionado';
-        $edat = $data['edat'] ?? 'No proporcionado';
-        $dni = $data['dni'] ?? 'No proporcionado';
-
-        // Imprimir los datos procesados
-        echo "Nombre: $nom <br>";
-        echo "Edad: $edat <br>";
-        echo "DNI: $dni <br>";
-    } else {
-        echo "Error al decodificar los datos JSON.";
-    }
-
-?>
+var_export($_POST);
