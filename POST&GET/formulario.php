fb7935d9da5b34d70b8292b335af52dfd8d3b754
adriancesurform <adrian.s465656@cesurformacion.com>
2024-11-19 09:52:11 +0100

    post

--- POST&GET/formulario.php
+++ POST&GET/formulario.php
@@ -1,8 +1,8 @@
 <?php
-if (isset($_POST['nombre']) && isset($_POST['dni']) && isset($_POST['edad'])) {
-    $nombre = htmlspecialchars($_POST['nombre']);
+if (isset($_POST['nom']) && isset($_POST['dni']) && isset($_POST['edat'])) {
+    $nombre = htmlspecialchars($_POST['nom']);
     $dni = htmlspecialchars($_POST['dni']);
-    $edad = htmlspecialchars($_POST['edad']);
+    $edad = htmlspecialchars($_POST['edat']);
 
     echo "Nombre: " . $nombre . "<br>";
     echo "DNI: " . $dni . "<br>";
