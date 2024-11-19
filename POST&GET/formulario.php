fac5074d8d7c6a5f2f61c8f0966834ea6fd74f7e
adriancesurform <adrian.s465656@cesurformacion.com>
2024-11-19 10:01:06 +0100

    post

--- POST&GET/formulario.php
+++ POST&GET/formulario.php
@@ -1,15 +1,13 @@
 <?php
 // Comprobar si se han recibido datos via POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recoger los datos enviados desde el formulario
-    $nom = $_POST['nom'];
-    $edat = $_POST['edat'];
-    $dni = $_POST['dni'];
+    // Comprobar si las variables POST están definidas y no están vacías
+    $nom = $_POST['nom'] ?? '';
+    $edat = $_POST['edat'] ?? '';
+    $dni = $_POST['dni'] ?? '';
 
     // Procesar los datos, por ejemplo, mostrar los valores
     echo "Nombre: " . htmlspecialchars($nom) . "<br>";
     echo "Edad: " . htmlspecialchars($edat) . "<br>";
     echo "DNI: " . htmlspecialchars($dni) . "<br>";
 }
-?>
-
