5321bdd48dbbc5d900f73247aa52f3aa8b6295a3
adriancesurform <adrian.s465656@cesurformacion.com>
2024-11-19 09:46:19 +0100

    post

--- POST&GET/formulario.php
+++ POST&GET/formulario.php
@@ -1,28 +1,11 @@
 <?php
-
-class Persona
-{
-    public $nombre;
-    public $dni;
-    public $edad;
-
-    public function __construct($nombre, $dni, $edad)
-    {
-        $this->nombre = $nombre;
-        $this->dni = $dni;
-        $this->edad = $edad;
-    }
-}
-
 if (isset($_POST['name']) && isset($_POST['dni']) && isset($_POST['edad'])) {
     $nombre = htmlspecialchars($_POST['name']);
     $dni = htmlspecialchars($_POST['dni']);
     $edad = htmlspecialchars($_POST['edad']);
 
-    $persona = new Persona($nombre, $dni, $edad);
-
-    echo $persona->nombre;
-    echo $persona->dni;
-    echo $persona->edad;
+    echo "Nombre: " . $nombre . "<br>";
+    echo "DNI: " . $dni . "<br>";
+    echo "Edad: " . $edad . "<br>";
 }
 
