4733a67c9815106f16b4c74b80d07f6ca5de5e8d
adriancesurform <adrian.s465656@cesurformacion.com>
2024-11-21 09:30:30 +0100

    tojson

--- POST&GET/exercici4.php
+++ POST&GET/exercici4.php
@@ -2,13 +2,14 @@
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $dades = json_decode($_POST['dades'], true);
+    $novesDades = json_decode($_POST['novesDades'], true);
 
     echo htmlspecialchars($dades['nom']) . "<br>";
     echo htmlspecialchars($dades['edat']) . "<br>";
     echo htmlspecialchars($dades['dni']) . "<br>";
     echo "<br>";
-    echo htmlspecialchars($dades['novesdades.noucamp']) . "<br>";
-    echo htmlspecialchars($dades['novesdades.nounumero']) . "<br>";
+    echo htmlspecialchars($novesDades['noucamp']) . "<br>";
+    echo htmlspecialchars($novesDades['nounumero']) . "<br>";
 
 } else {
     echo 'Sin datos';
